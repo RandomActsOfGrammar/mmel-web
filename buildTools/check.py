ef332160a7f0898f24370826fd73ebe89bc1774d
~ <demichaelson@gmail.com>
2023-12-24 14:26:31 -0600

    Add script to check files are present; fix relative link error

---
+++ buildTools/check.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import os
+import sys
+
+#check all the files
+def check_all(root):
+    messages = []
+    for f in os.listdir(root):
+        full = root + "/" + f
+        if os.path.isdir(full):
+            messages += check_all(full)
+        elif f.endswith(".html"):
+            openFile = open(full, 'r')
+            fContents = openFile.read()
+            openFile.close()
+            messages += check_file(fContents, f, root)
+    return messages
+
+
+#check all links in a file for validity, returning a list of messages
+#for missing files
+def check_file(fContents, filename, dirname):
+    output = []
+    while fContents != "":
+        found_href = fContents.find('href="')
+        found_src = fContents.find('src="')
+        if found_href >= 0 and found_src >= 0:
+            if found_href < found_src: #href first
+                fContents = fContents[found_href + 6:]
+            else: #src first
+                fContents = fContents[found_src + 5:]
+            quotes = fContents.find('"')
+            lookForName = fContents[:quotes]
+            full = dirname + "/" + lookForName
+            if  not (lookForName.startswith("https://") or \
+                     lookForName.startswith("http://")) and \
+                not os.path.isfile(full):
+                output += ["'" + lookForName + "' missing (linked in '" +
+                           dirname + "/" + filename + "')"]
+            fContents = fContents[quotes + 1:]
+        else:
+            fContents = ""
+    return output
+
+
+def main(args):
+    if len(args) < 2:
+        print("Must give directory to check")
+    msgs = check_all(args[1])
+    if len(msgs) > 0:
+        print("Missing files linked in website files:")
+        for m in msgs:
+            print("   " + m)
+        return 1
+    else:
+        print("No missing files; good to place")
+        return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
