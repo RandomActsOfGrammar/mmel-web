92bf96a3b332698412587743fa0a337d1a1fd7c4
~ <demichaelson@gmail.com>
2024-02-19 09:05:47 -0600

    Fix missing links and missing-link checker

--- buildTools/check.py
+++ buildTools/check.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import re
 
 #check all the files
 def check_all(root):
@@ -17,24 +18,48 @@ def check_all(root):
             messages += check_file(fContents, f, root)
     return messages
 
+#regex for matching an href or src in a file
+REF_RE = r'(([hH][rR][eE][fF])|([sS][rR][cC]))' + \
+         r'[\ ]*=[\ ]*"([^"#]*)(#[^"]*)?"'
 
 #check all links in a file for validity, returning a list of messages
 #for missing files
 def check_file(fContents, filename, dirname):
     output = []
+    l = re.findall(REF_RE, fContents)
+    for _, _, _, lookForName, tag in l:
+        full = dirname + "/" + lookForName
+        if  not (lookForName.startswith("https://") or \
+                 lookForName.startswith("http://")) and \
+            not lookForName == "javascript:void" and \
+            not lookForName == "" and \
+            not os.path.isfile(full):
+            output += ["'" + lookForName + "' missing (linked in '" +
+                       dirname + "/" + filename + "')"]
+    return output
+
+
+#check all links in a file for validity, returning a list of messages
+#for missing files
+def check_file2(fContents, filename, dirname):
+    output = []
     while fContents != "":
         found_href = fContents.find('href="')
         found_src = fContents.find('src="')
-        if found_href >= 0 and found_src >= 0:
-            if found_href < found_src: #href first
+        if found_href >= 0 or found_src >= 0:
+            if found_href < found_src or found_src == -1: #href first
                 fContents = fContents[found_href + 6:]
             else: #src first
                 fContents = fContents[found_src + 5:]
             quotes = fContents.find('"')
             lookForName = fContents[:quotes]
+            if '#' in lookForName: #remove id on page
+                lookForName = lookForName[:lookForName.rfind('#')]
             full = dirname + "/" + lookForName
             if  not (lookForName.startswith("https://") or \
                      lookForName.startswith("http://")) and \
+                not lookForName == "javascript:void" and \
+                not lookForName == "" and \
                 not os.path.isfile(full):
                 output += ["'" + lookForName + "' missing (linked in '" +
                            dirname + "/" + filename + "')"]
